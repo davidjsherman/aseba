3e96aaa800d8b7561950dab27e0a177b2b5da1a7
davidjsherman <david.sherman@inria.fr>
2017-06-28 23:53:46 +0200

    basic verification zeroconf is a container

--- tests/zeroconf/test-zeroconf.cpp
+++ tests/zeroconf/test-zeroconf.cpp
@@ -9,7 +9,7 @@
 */
 
 #define CATCH_CONFIG_MAIN  // Ask Catch to provide a main()
-#include "catch.hpp"       // Catch is header-only
+#include "../catch.hpp"       // Catch is header-only
 #if defined(_WIN32) && defined(__MINGW32__)
 // Avoid conflict from /mingw32/.../include/winerror.h */
 #undef ERROR_STACK_OVERFLOW
@@ -18,71 +18,81 @@
 #include "../../common/zeroconf/zeroconf.h"
 
 /*
+ Setup: create Dashel Stream to imitate tcp: target
+ */
+#include <dashel/dashel.h>
+class MockHub: public Dashel::Hub
+{
+	Dashel::Stream* stream;
+public:
+	Dashel::Stream* listen() { return stream = Dashel::Hub::connect("tcpin:port=0"); }
+} mock;
+Dashel::Stream* mock_stream = mock.listen();
+
+/*
   Use Catch to write tests in Behavior-driven design (BDD) style
 */
 
 SCENARIO( "Aseba::Zeroconf is a container", "[container]" ) {
-  Aseba::Zeroconf zs;
-    
-  GIVEN( "Zeroconf collection was initialized" ) {
-    REQUIRE( zs.size() == 0 );
-    REQUIRE( zs.find("fizbin") == zs.find.end() );
+//	auto fizbin_i = zs.begin();
+	
+	GIVEN( "Zeroconf collection was created" ) {
+		Aseba::Zeroconf zs;
 
-    WHEN( "target is inserted by name" ) {
-      zs.insert();
-      auto fizbin_i = zs.find("fizbin");
-      THEN( "collection not empty and contains target" ) {
-	REQUIRE( zs.size() == 1 );
-	REQUIRE( fizbin_i != zs.find.end() );
-	REQUIRE( fizbin_i.name.find("fizbin") == 0 );
-      }
-    }
-    
-    WHEN( "target txtrecord is added" ) {
-      auto fizbin_i = zs.find("fizbin");
-      Aseba::Zeroconf::TxtRecord txt{5, "fizbin", {1,2}, {99,99} };
-      fizzbin_i.updateTxtRecord(txt);
-      THEN( "target txtrecord is updated" ) {
-	REQUIRE( fizbin_i.name.ids.size() == 2 );
-	REQUIRE( fizbin_i.name.ids.pids() == 2 );
-      }
-    }
-    
-    WHEN( "target is inserted by stream" ) {
-      zs.insert();
-      auto foobar_i = zs.find("foobar");
-      THEN( "collection not empty and contains fizbin" ) {
-	REQUIRE( zs.size() == 2 );
-	REQUIRE( foobar_i != zs.find.end() );
-	REQUIRE( foobar_i.name.find("foobar") == 0 );
-      }
-    }
-
-    WHEN( "targets are traversed with a loop" ) {
-      for (auto & target: zs)
-	{
-	  REQUIRE( target.regtype.find("_aseba._tcp") == 0 );
-	  REQUIRE( target.domain.find("local.") == 0 );
-	  REQUIRE( target.local == true );
+		REQUIRE( zs.size() == 0 );
+		REQUIRE( zs.find("fizbin") == zs.end() );
+		
+		WHEN( "target is inserted by name" ) {
+			zs.insert("Fizbin", 33333);
+			auto fizbin_i = zs.find("Fizbin");
+			THEN( "collection not empty and contains target" ) {
+				REQUIRE( zs.size() == 1 );
+				REQUIRE( fizbin_i != zs.end() );
+				REQUIRE( fizbin_i->name.find("Fizbin") == 0 );
+			}
+			
+			//			fizbin_i = zs.find("Fizbin");
+			//			REQUIRE( fizbin_i != zs.end() );
+			//			Aseba::Zeroconf::TxtRecord txt{5, "Fizbin Foobar", {1,2}, {99,99} };
+			//			fizbin_i->updateTxtRecord(txt);
+			//			THEN( "target txtrecord is updated" ) {
+			//				REQUIRE( fizbin_i->properties["ids"].size() == 2 );
+			//				REQUIRE( fizbin_i->properties["pids"].size() == 2 );
+			//			}
+			
+			WHEN( "targets is inserted from stream" ) {
+				zs.insert(mock_stream);
+				auto foobar_i = zs.find("Aseba Local");
+				THEN( "collection not empty and contains fizbin" ) {
+					REQUIRE( zs.size() == 2 );
+					REQUIRE( foobar_i != zs.end() );
+					REQUIRE( foobar_i->name.find("Aseba Local") == 0 );
+				}
+				
+				WHEN( "targets are traversed with a loop" ) {
+					for (auto & target: zs)
+					{
+						REQUIRE( target.regtype.find("_aseba._tcp") == 0 );
+						REQUIRE( target.domain.find("local.") == 0 );
+						REQUIRE( target.local == true );
+					}
+					
+					WHEN( "target is erased" ) {
+						zs.erase(zs.begin()+1);
+						auto fizbin_i = zs.find("Fizbin");
+						THEN( "collection contains Fizbin but not Aseba Local" ) {
+							REQUIRE( zs.size() == 1 );
+							REQUIRE( fizbin_i != zs.end() );
+							REQUIRE( fizbin_i->name.find("Fizbin") == 0 );
+						}
+						
+						WHEN( "target is cleared" ) {
+							zs.clear();
+							REQUIRE( zs.size() == 0 );
+						}
+					}
+				}
+			}
+		}
 	}
-    }
-
-    WHEN( "target is erased" ) {
-      zs.erase(1);
-      auto fizbin_i = zs.find("fizbin");
-      auto foobar_i = zs.find("foobar");
-      THEN( "collection contains foobar but not fizbin" ) {
-	REQUIRE( zs.size() == 1 );
-	REQUIRE( fizbin_i != zs.find.end() );
-	REQUIRE( foobar_i == zs.find.end() );
-	REQUIRE( fizbin_i.name.find("fizbin") == 0 );
-      }
-    }
-
-    WHEN( "target is cleared" ) {
-      zs.clear();
-      REQUIRE( zs.size() == 0 );
-    }
-    
-  }
 }
